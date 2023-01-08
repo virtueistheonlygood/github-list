58a1d22a4f92fbbd110536c38f48ace789d4ce57
Mariano Julian Santos <mariano.santos@Mariano-Julian-Santos.local>
2023-01-08 06:39:47 +0100

    github-list.sh

---
+++ github-list.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for file in $(git ls-files | grep -v README.md)
+do
+	HASHOLD=$(git rev-list HEAD --date-order "$file" | tail -n 1)
+	HASHLST=$(git rev-list HEAD --date-order --reverse "$file" | tail -n 1)
+	DATEOLD=$(git show -s --format="%f, %ci, %an," $HASHOLD --)
+	DATELST=$(git show -s --format="%f, %ci, %an" $HASHLST --)
+	printf "%s, %s\n" "$file" "$DATEOLD $DATELST"
+done
